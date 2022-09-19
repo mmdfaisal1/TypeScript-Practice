2e009d8773cdef3f986aaf11144cd899c045656f
huhammadfaisal <faisal.muhammad@contrastsecurity.com>
2022-09-19 13:22:11 -0500

    more prototype

--- prototype.ts
+++ prototype.ts
@@ -9,12 +9,13 @@ console.log(Object.getPrototypeOf(myDate));
 
 //Setting a prototype
 //https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes#setting_a_prototype
+//Using Object.create()
 const personPrototype = {
   firstName: "John",
   lastName: "Doe",
   id: 5566,
   greet() {
-    console.log("Hello!");
+    console.log("Hello from personPrototype1");
   },
   fullName: function () {
     console.log(`${this.firstName}  ${this.lastName}`);
@@ -23,7 +24,23 @@ const personPrototype = {
 
 const person1 = Object.create(personPrototype);
 console.log(person1.firstName); //John
-person1.greet(); //Hello!
+person1.greet(); //Hello from personPrototype1
+
+//Using constructor
+const personPrototype2 = {
+  greet() {
+    console.log(`Hello from personPrototype2`);
+  },
+};
+
+function Person(this: any, name: string) {
+  this.name = name;
+}
+
+Object.assign(Person.prototype, personPrototype2);
+
+const faisal = new (Person as any)("faisal");
+faisal.greet(); //Hello from personPrototype2
 
 //Object can also be created by using object constructor
 let y = new Object();
