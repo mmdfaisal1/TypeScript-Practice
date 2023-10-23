4fa7153962066d2e3724ff17284aba022d4b51ad
Muhammad Faisal <faisal.muhammad@contrastsecurity.com>
2023-10-22 21:24:57 -0500

    changes

--- arrays.ts
+++ arrays.ts
@@ -1,10 +1,10 @@
-export{}
+export {};
 
 const person = {
-    name: "John",
-    age: 30,
-    hobbies: ["Sports", "Cooking"]
-}
+  name: "John",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+};
 
 let favoriteActivities: string[];
 favoriteActivities = ["Music", "Reading"];
@@ -12,16 +12,25 @@ favoriteActivities = ["Music", "Reading"];
 //Because TypeScript knows 'activity' is a string
 //We can perform any string functions on it
 for (const activity of favoriteActivities) {
-    console.log(activity.toUpperCase());
+  console.log(activity.toUpperCase());
 }
 
+favoriteActivities.forEach((element) => {
+  console.log(element);
+});
+
+favoriteActivities.forEach(function (item) {
+  console.log(item);
+});
+
+function PrintHello() {
+  console.log("Helloooooo!");
+}
+
+favoriteActivities.forEach(PrintHello); //Will print "Helloooo!" for each item in the array
 
 for (const key in favoriteActivities) {
-    console.log(favoriteActivities[key]);
+  console.log(favoriteActivities[key]);
 }
 
 let anotherArray: any[]; //Not advisable, since you lose the power of TypeScript
-
-
-
-
